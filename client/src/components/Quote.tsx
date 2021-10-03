ae9b02ff68da0c3333f5d25415170df5e56dc6ce
Thanathip Suwannakhot <dannajaeiei@hotmail.com>
2021-10-03 15:17:32 +0700

    create component quote and fetching data from backend

--- client/src/components/Quote.tsx
+++ client/src/components/Quote.tsx
@@ -1,16 +1,30 @@
-import { useState } from 'react'
+import { useState } from "react";
+import axios from "axios";
+import "./quote.css";
 
 const Quote = () => {
+
   const [text, setText] = useState("");
   const [author, setAuthor] = useState("");
+  const [isGenerated, setGenerate] = useState(false);
+
+  const getQuote = () => {
+    axios.get("http://localhost:5000/")
+      .then((response) => {
+        setText(response.data['text']);
+        setAuthor(response.data['author']);
+        setGenerate(true);
+      })
+      .catch((error) => console.log(error));
+  };
 
   return (
     <div>
-      <button>Generate Quote</button>
-      <h1>{text}</h1>
-      <h3>{"-" + author}</h3>
+      <button onClick={getQuote}>Generate Quote</button>
+        <h1>{text}</h1>
+        <h3><span className={!isGenerated ? "hide" : ""}>- </span>{author}</h3>
     </div>
-  )
-}
+  );
+};
 
 export default Quote;
